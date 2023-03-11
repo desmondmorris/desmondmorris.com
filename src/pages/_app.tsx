56acea4c3a561b72b36c9ddad0cd132906360cbe
Desmond Morris <hi@desmondmorris.com>
2023-03-11 13:00:42 -0500

    Add google analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,17 @@
 import type { AppProps } from 'next/app'
 import { ChakraProvider } from '@chakra-ui/react'
+import { GoogleAnalytics } from "nextjs-google-analytics"
 
 import '@fontsource/roboto-slab/400.css'
 import '@fontsource/roboto-slab/600.css'
 import '@fontsource/open-sans/600.css'
-
 
 import theme from '../styles/theme'
 
 function App({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
+      <GoogleAnalytics trackPageViews />
       <Component {...pageProps} />
     </ChakraProvider>
   )
